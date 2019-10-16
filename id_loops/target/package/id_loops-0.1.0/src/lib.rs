09a7c3a4f35d481dffe9eeb4e2ed6129eb7ee5ab
Amna <amnakhan8685@gmail.com>
2019-10-16 13:07:12 +0500

    version bhi barha dia ab to

--- id_loops/target/package/id_loops-0.1.0/src/lib.rs
+++ id_loops/target/package/id_loops-0.1.0/src/lib.rs
@@ -1,4 +1,4 @@
-fn for_loop(start:u8,end:u8){
+pub fn for_loop(start:u8,end:u8){
     for index in start..end{
         println!("{}",index);
     }
---
+++ id_loops/target/rls/debug/deps/id_loops-237a7a6dc3fa055e.d
@@ -0,0 +1,5 @@
+/home/usama/Desktop/rust libraries/aamjad/id_loops/target/rls/debug/deps/id_loops-237a7a6dc3fa055e.rmeta: src/lib.rs
+
+/home/usama/Desktop/rust libraries/aamjad/id_loops/target/rls/debug/deps/id_loops-237a7a6dc3fa055e.d: src/lib.rs
+
+src/lib.rs:
